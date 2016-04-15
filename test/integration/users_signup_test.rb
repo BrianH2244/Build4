193499783e615a28e5e43d4d970967a1dae5e8ac
BrianH2244 <freebigb@hotmail.com>
2016-04-15 19:04:03 +0000

    Removed extra tests

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -21,32 +21,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
   test "valid signup information with account activation" do
     get signup_path
-    name  = "Example User"
-    email = "user@example.com"
-    password = "foobar"
     assert_difference 'User.count', 1 do
-      post users_path, user: { name:  name,
-                               email: email,
-                               password:              password,
-                               password_confirmation: password }
+      post users_path, user: { name:  "Example User",
+                               email: "user@example.com",
+                               password:              "password",
+                               password_confirmation: "password" }
     end
-    assert_equal ActionMailer::Base.deliveries.size, 1
+    assert_equal 1, ActionMailer::Base.deliveries.size
     user = assigns(:user)
     assert_not user.activated?
     # Try to log in before activation.
     log_in_as(user)
     assert_not is_logged_in?
-    # Index page
-    # Log in as valid user.
-    log_in_as(users(:michael))
-    # Unactivated user is not on the second page
-    get users_path, page: 2
-    assert_no_match user.name, response.body
-    # Profile page
-    get user_path(user)
-    assert_redirected_to root_url
-    # Log out valid user.
-    delete logout_path
     # Invalid activation token
     get edit_account_activation_path("invalid token")
     assert_not is_logged_in?
@@ -58,6 +44,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert user.reload.activated?
     follow_redirect!
     assert_template 'users/show'
+    assert_not_empty flash
     assert is_logged_in?
   end
 end
